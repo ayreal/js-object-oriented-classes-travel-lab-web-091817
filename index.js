156345f312439dd0392b9629c611be4b3690f093
Ariel Herman <aeh.herman@gmail.com>
2017-10-31 20:10:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    // debugger;
+    let elapsed = new Date(year, 1, 1) - this.startDate;
+    elapsed = elapsed / (1000 * 60 * 60 * 24 * 365);
+    return Math.round(elapsed);
+  }
+}
+
+class Route {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  blocksTravelled() {
+    let eastWest = [
+      "1st Avenue",
+      "2nd Avenue",
+      "3rd Avenue",
+      "Lexington Avenue",
+      "Park",
+      "Madison Avenue",
+      "5th Avenue"
+    ];
+    let distVert = this.end.vertical - this.start.vertical;
+    let distHoriz =
+      eastWest.lastIndexOf(this["end"]["horizontal"]) -
+      eastWest.lastIndexOf(this["start"]["horizontal"]);
+    return Math.abs(distVert) + Math.abs(distHoriz);
+  }
+
+  estimatedTime(peak) {
+    // debugger;
+    if (peak) {
+      debugger;
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+}
